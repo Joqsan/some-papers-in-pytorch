2c3265a94552032353b4dc6558f6a167624c8e62
Joqsan Azocar <joqsan.a@gmail.com>
2023-01-11 17:46:36 +0300

    refactor ddpm/ddpm.py

--- ddpm/ddpm.py
+++ ddpm/ddpm.py
@@ -1,6 +1,8 @@
-from torch import nn
-import torch
+from typing import Union
+
 import numpy as np
+import torch
+from torch import nn
 
 
 class DDPMSampler(nn.Module):
@@ -8,52 +10,56 @@ class DDPMSampler(nn.Module):
         self,
         beta_1: float = 0.001,
         beta_T: float = 0.02,
-        device: str = "cuda",
+        device: Union[str, torch.device] = None,
     ) -> None:
         super().__init__()
+        self.device = device
 
         max_num_timesteps = 1000
 
-        self.betas = torch.linspace(beta_1, beta_T, max_num_timesteps)
+        self.betas = torch.linspace(
+            beta_1, beta_T, max_num_timesteps, device=self.device
+        )
 
         self.alphas = 1 - self.betas
-        self.alphas_prod = torch.cumprod(self.alphas, dim=0)
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
 
         # discrete timesteps
         self.timesteps = torch.from_numpy(
             np.arange(0, max_num_timesteps)[::-1].copy()
-        ).to(device)
+        ).to(self.device)
 
     def _get_sigma(self, t: int) -> torch.Tensor:
-        alpha_prod_prev = self.alphas_prod[t - 1] if t > 0 else torch.tensor(1.0)
-        alpha_prod_curr = self.alphas_prod[t]
+        alpha_cumprod_t_prev = self.alphas_cumprod[t - 1] if t > 0 else torch.tensor(1.0)
+        alpha_cumprod_t = self.alphas_cumprod[t]
 
         # variance for a deterministic x_0 (see section 3.2.)
-        variance = (1 - alpha_prod_prev) / (1 - alpha_prod_curr) * self.betas[t]
-        sigma = torch.sqrt(variance)
+        variance_t = (1 - alpha_cumprod_t_prev) / (1 - alpha_cumprod_t) * self.betas[t]
+        sigma_t = torch.sqrt(variance_t)
 
-        return sigma
+        return sigma_t
 
     def sample(
         self, eps_theta_t: torch.Tensor, t: torch.Tensor, x_t: torch.Tensor
     ) -> torch.Tensor:
         alpha_t = self.alphas[t]
-        alpha_prod_t = self.alphas_prod[t]
+        alpha_cumprod_t = self.alphas_cumprod[t]
 
-        # See formula (11)
+        # See eq. (11)
         mu_theta_t = (
             1
             / alpha_t**0.5
-            * (x_t - (1 - alpha_t) / (1 - alpha_prod_t) ** 0.5 * eps_theta_t)
+            * (x_t - (1 - alpha_t) / (1 - alpha_cumprod_t) ** 0.5 * eps_theta_t)
         )
 
-        z = torch.randn(x_t.shape) if t > 0 else torch.zeros_like(x_t)
-        z = z.to(x_t.device)
-
+        z = (
+            torch.randn(x_t.shape, device=self.device)
+            if t > 0
+            else torch.zeros_like(x_t, device=self.device)
+        )
         sigma_t = self._get_sigma(t)
-        sigma_t = sigma_t.to(x_t.device)
 
-        # See algorithm (2) and formula (1)
+        # See algorithm (2) and eq. (1)
         x_t_prev = mu_theta_t + sigma_t * z
 
         return x_t_prev
